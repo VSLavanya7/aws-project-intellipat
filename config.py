6e06d98199f4a2de0e70a76b946787a6618fbfab
Lavanya <lavanyavijapurapu@gmail.com>
2026-08-04 13:36:22 +0200

    commiting all the final changes

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # config.py
 
-customhost = "employee.cfg0sq4cowsw.eu-north-1.rds.amazonaws.com"
-customuser = "dbadmin"
-custompass = "Admin123"
+customhost = "aws-project-e2e.cfg0sq4cowsw.eu-north-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "Welcome123"
 customdb = "employee"
 custombucket = "addemploye-261247365620"
 customregion = "eu-north-1"   # Replace with your AWS Region
